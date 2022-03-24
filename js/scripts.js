56ac9f104195dc855214e4fc75fd8f3fa5b6bd0d
Jarret Jeter <jarretjeter@yahoo.com>
2022-03-24 10:47:43 -0700

    add ofAge function to check user's age and whether or not to show certain html elements
    co-authored-by: Timothy RYAN <ryaninlux@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
-$(document).ready(function() {  $("h1").click(function() {
+function ofAge(ageInput) {
+  if (ageInput >= 18) {
+    $('.over18').show();
+  } else {
+    $('.under18').show();
+  }
+};
+
+$(document).ready(function() {
   
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    const ageInput = parseInt($("input#age").val());
+    $("#age").text(ageInput);
+    console.log(ofAge(ageInput));
+  })
 });
 
 
