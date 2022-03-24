e666ba86ee1db091f14934558df1cf36e8e2746d
Jarret Jeter <jarretjeter@yahoo.com>
2022-03-24 09:15:55 -0700

    added basic html text
    co-authored-by: Timothy RYAN <ryaninlux@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,5 @@
-$(document).ready(function() {
+$(document).ready(function() {  $("h1").click(function() {
   
-  $("h1").click(function() {
-    alert("This is a heading.");
-  });
-
-  $("p").click(function() {
-    alert("This is a paragraph.");
-  });
-
-  $("img").click(function() {
-    alert("This is an image.");
-  });
 });
 
 
